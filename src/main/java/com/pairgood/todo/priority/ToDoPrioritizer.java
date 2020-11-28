af6c7db44b23333544efd4fda240d82e6d84e8cd
dkessler <dkessler@pillartechnology.com>
2020-11-28 15:38:40 -0600

    refactor: removed duplicate for loops

--- src/main/java/com/pairgood/todo/priority/ToDoPrioritizer.java
+++ src/main/java/com/pairgood/todo/priority/ToDoPrioritizer.java
@@ -11,28 +11,17 @@ public class ToDoPrioritizer implements Prioritizer<ToDo, Long> {
     public List<ToDo> prioritize(List<ToDo> toDos, Long targetPriority, Long amount) {
         List<ToDo> reprioritizedToDos = new ArrayList<>();
 
-        if (amount > 0) {
-            for (ToDo toDo : toDos) {
-                long priority = toDo.getPriority();
-                if (priority == targetPriority) {
-                    toDo.setPriority(priority - amount);
-                } else if (priority > (targetPriority - amount - 1) && priority < targetPriority) {
-                    toDo.setPriority(priority + 1);
-                }
-
-                reprioritizedToDos.add(toDo);
+        for (ToDo toDo : toDos) {
+            long priority = toDo.getPriority();
+            if (priority == targetPriority) {
+                toDo.setPriority(priority - amount);
+            } else if (amount > 0 && priority > (targetPriority - amount - 1) && priority < targetPriority) {
+                toDo.setPriority(priority + 1);
+            } else if (priority > (targetPriority - 1) && priority < (targetPriority - amount + 1)) {
+                toDo.setPriority(priority - 1);
             }
-        } else {
-            for (ToDo toDo : toDos) {
-                long priority = toDo.getPriority();
-                if (priority == targetPriority) {
-                    toDo.setPriority(priority - amount);
-                } else if (priority > (targetPriority - 1) && priority < (targetPriority - amount + 1)) {
-                    toDo.setPriority(priority - 1);
-                }
 
-                reprioritizedToDos.add(toDo);
-            }
+            reprioritizedToDos.add(toDo);
         }
 
         reprioritizedToDos.sort((ToDo first, ToDo second) -> (int) (first.getPriority() - second.getPriority()));
